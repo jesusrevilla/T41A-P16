854da268f92e275e67a507ba45fa4fc48a7e0f20
Emmanuel  Hernandez Mendez <179169@upslp.edu.mx>
2025-11-10 19:21:49 -0600

    Feature/179169

---
+++ 02_insert_data.sql
@@ -0,0 +1,7 @@
+-- Insertar productos iniciales
+INSERT INTO productos (nombre, stock, precio_unitario)
+VALUES
+('Tornillo', 100, 0.50),
+('Tuerca', 200, 0.30),
+('Arandela', 150, 0.20),
+('Clavo', 300, 0.10);
