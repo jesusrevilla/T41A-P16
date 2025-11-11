9f477061d9fcca2e1a6f308bed71766129d4c96c
Coral024 <202579467+Coral024@users.noreply.github.com>
2025-11-10 18:39:07 -0600

    tests and update procedure calls

--- tests/test_bd.py
+++ tests/test_bd.py
@@ -24,28 +24,24 @@ def run_query_from_file(conn, filename):
         cur.execute(query)
         return cur.fetchall()
 
-def test_function_discount(db_connection):
-    prod_org = 100.00
-    descuento = 10.00
-    expected = prod_org - (prod_org * (descuento / 100.00))
+def test_function_procedure(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT desc_producto(%s, %s);", (prod_org, descuento))
+        cur.execute("CALL registrar_movimiento(1, 'salida', 20);")
+        result = cur.fetchone()[0]
+        cur.execute("CALL registrar_movimiento(2, 'entrada', 50);")
+        result2 = cur.fetchone()[0]
+    assert result == 80
+    assert result2 == 250
+
+def test_function(db_connection):
+    expected=115.00
+    with db_connection.cursor() as cur:
+        cur.execute("SELECT calcular_valor_inventario();")
         result = cur.fetchone()[0]
     assert float(result) == pytest.approx(expected)
 
-def test_function_email(db_connection):
+def test_trigger(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT valida_correo('corjagamil.com');")
-        result = cur.fetchone()[0]
-    assert result is False
-
-def test_function_stock(db_connection):
-    expected = [
-        ('Camara', 5),
-        ('Computadora', 8),
-        ('Telefono', 15)
-        ]
-    with db_connection.cursor() as cur:
-        cur.execute("SELECT * FROM stock_prod(20);")
+        cur.execute("SELECT * FROM auditoria_stock;")
         result = cur.fetchall()
-    assert set(result) == set(expected)
+    assert len(result) == 2
