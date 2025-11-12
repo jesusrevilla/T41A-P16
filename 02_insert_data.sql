6c8fe60e5d2a8ede62c01b69a4bef18110a78794
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-11-12 08:07:39 -0600

    feature/182483

---
+++ 02_insert_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO productos (nombre, categoria, descripcion, stock, precio_unitario) VALUES
+('Cable USB', 'Electrónica', 'Cable USB-A a USB-C 1m', 50, 2.50),
+('Adaptador HDMI', 'Electrónica', 'Adaptador HDMI a VGA', 30, 5.00);
