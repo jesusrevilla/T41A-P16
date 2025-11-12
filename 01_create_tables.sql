2618ddbebe81cf7c36511a1c46332070850d1589
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-11-12 09:50:11 -0600

    feature/182483

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,28 +1,22 @@
 CREATE TABLE productos (
     id SERIAL PRIMARY KEY,
     nombre TEXT NOT NULL,
-    categoria TEXT,
-    descripcion TEXT,
-    stock INT NOT NULL CHECK (stock >= 0),
-    precio_unitario NUMERIC(10,2) NOT NULL CHECK (precio_unitario >= 0),
-    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    stock INT NOT NULL,
+    precio_unitario NUMERIC(10,2) NOT NULL
 );
-
 
 CREATE TABLE movimientos_inventario (
     id SERIAL PRIMARY KEY,
-    producto_id INT NOT NULL REFERENCES productos(id) ON DELETE CASCADE,
-    tipo_movimiento TEXT NOT NULL CHECK (tipo_movimiento IN ('entrada', 'salida')),
-    cantidad INT NOT NULL CHECK (cantidad > 0),
-    usuario TEXT,
-    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    producto_id INT REFERENCES productos(id),
+    tipo_movimiento TEXT CHECK (tipo_movimiento IN ('entrada', 'salida')),
+    cantidad INT NOT NULL,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
 
 CREATE TABLE auditoria_stock (
     id SERIAL PRIMARY KEY,
-    producto_id INT NOT NULL,
-    stock_anterior INT NOT NULL,
-    stock_nuevo INT NOT NULL,
-    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    producto_id INT,
+    stock_anterior INT,
+    stock_nuevo INT,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
