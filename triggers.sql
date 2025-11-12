52a1c1c169e369ed6bcb6deea0009c8f83c46f39
SebasMonreal11 <125666778+SebasMonreal11@users.noreply.github.com>
2025-11-11 19:10:17 -0600

    Create triggers.sql

---
+++ triggers.sql
@@ -0,0 +1,16 @@
+-- Función y trigger para auditoría de cambios en el stock
+
+CREATE OR REPLACE FUNCTION registrar_auditoria_stock()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO auditoria_stock (producto_id, stock_anterior, stock_nuevo)
+    VALUES (OLD.id, OLD.stock, NEW.stock);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_auditoria_stock
+AFTER UPDATE OF stock ON productos
+FOR EACH ROW
+WHEN (OLD.stock IS DISTINCT FROM NEW.stock)
+EXECUTE FUNCTION registrar_auditoria_stock();
