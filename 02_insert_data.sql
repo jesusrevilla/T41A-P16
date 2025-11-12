25306331ba3beccc106165ddeaae6b5667b306a2
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-11-12 09:50:31 -0600

    feature/182483

--- 02_insert_data.sql
+++ 02_insert_data.sql
@@ -1,3 +1,2 @@
-INSERT INTO productos (nombre, categoria, descripcion, stock, precio_unitario) VALUES
-('Cable USB', 'Electrónica', 'Cable USB-A a USB-C 1m', 50, 2.50),
-('Adaptador HDMI', 'Electrónica', 'Adaptador HDMI a VGA', 30, 5.00);
+INSERT INTO productos (nombre, stock, precio_unitario) VALUES ('Tornillo', 100, 0.50);
+INSERT INTO productos (nombre, stock, precio_unitario) VALUES ('Tuerca', 200, 0.30);
