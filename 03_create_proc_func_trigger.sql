857adba56931683bf4bb30350a875a8e731893ca
Uriel4443 <182829@upslp.edu.mx>
2025-11-11 19:24:28 -0600

    Update 03_create_proc_func_trigger.sql

--- 03_create_proc_func_trigger.sql
+++ 03_create_proc_func_trigger.sql
@@ -36,3 +36,19 @@ BEGIN
     RETURN v_total;
 END;
 $$ LANGUAGE plpgsql;
+
+--Trigger
+CREATE OR REPLACE FUNCTION registrar_auditoria_stock()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO auditoria_stock (producto_id, stock_anterior, stock_nuevo)
+    VALUES (OLD.id, OLD.stock, NEW.stock);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_auditoria_stock
+AFTER UPDATE OF stock ON productos
+FOR EACH ROW
+WHEN (OLD.stock IS DISTINCT FROM NEW.stock)
+EXECUTE FUNCTION registrar_auditoria_stock();
